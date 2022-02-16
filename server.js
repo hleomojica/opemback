34fa48c5b12c35c73a044480877f39aeaf79fd6f
Jorge Mojica <jorge.mojica92@gmail.com>
2022-02-15 23:00:55 -0500

    cambios

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "https://app.opem.com.co");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
